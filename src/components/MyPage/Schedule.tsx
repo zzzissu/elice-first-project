129f0c3b23c47462491c052b8c6858a7d00e9c00
[React1]하정우 <hjh268100@gmail.com>
2024-10-10 22:41:26 +0900

    [feat]지언님 파일

--- src/components/MyPage/Schedule.tsx
+++ src/components/MyPage/Schedule.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Schedule = () => {
   return (
-    <div>
-      Schedule
+    <div className='sectionDevide flex flex-row'>
+      {/*왼쪽 구역 */}
+      <div className='leftSite flex flex-col'>
+안녕
+
+
+      </div>
+      {/*오른쪽 구역 */}
+      <div className='rightSite'>
+안녕
+      </div>
     </div>
   )
 }
