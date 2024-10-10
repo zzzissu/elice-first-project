6d92168d9119c8652462f2668ee2fd1199dc43aa
[React1] 최준영 <happy77523@naver.com>
2024-10-10 10:23:49 +0900

    Refactor: Nav바 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-"./src/*/.{ts,tsx}"],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      sans: ['Noto Sans', 'ui-sans-serif', 'system-ui'],},
+      colors:{
+        customGray:'rgb(95 , 99 , 104)'
+      }
   },
   plugins: [],
 }
-
+}
