a6d9a7b09dd2285bb4dd301f451a23fe3b9cde3b
[React1]하정우 <hjh268100@gmail.com>
2024-10-17 15:18:20 +0900

    [feat]api 올리기

--- src/components/myPage/ProfileEdit.tsx
+++ src/components/myPage/ProfileEdit.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import FormModal from '../modal/FormModal';
+import DeleteModal from '../modal/DeleteModal';
 
 
 const ProfileEdit = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [phone, setPhone] = useState('');
     const [name, setName] = useState("");
     const [birth, setBirth] = useState("");
     const [department, setDepartment] = useState("");
@@ -14,6 +16,33 @@ const ProfileEdit = () => {
     useEffect(() => {
         fetchUserData();
     }, [])
+
+    const userDelete = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                console.error("인증 토큰이 없습니다. 로그인 상태를 확인하세요.");
+                return;
+            }
+            const res = await fetch("http://localhost:4000/api/users/delete", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+
+            if(res.ok){
+                console.error("삭제실패하였습니다.", res.status)
+                return;
+            }
+            alert("삭제되었습니다.");
+            window.location.href = "/login"
+
+        } catch (e) {
+            console.error("오류가 발생되었습니다.", e)
+        }
+    }
 
     const fetchUserData = async () => {
         try {
@@ -25,7 +54,8 @@ const ProfileEdit = () => {
             }
             const res = await fetch("http://localhost:4000/api/users", {
                 method: "GET",
-                headers: { "Content-Type": "application/json" ,
+                headers: {
+                    "Content-Type": "application/json",
                     "Authorization": `Bearer ${token}`,
                 },
             });
@@ -33,15 +63,16 @@ const ProfileEdit = () => {
             if (res.ok) {
                 const data = await res.json();
                 setName(data.name);
-                setPhoneNumber(data.phone);
+                setPhone(data.phone);
                 setBirth(data.birth)
                 setDepartment(data.department);
                 setPosition(data.position);
             } else {
+                console.log(res.status)
                 console.error("사용자의 정보를 가져오는데 실패하였습니다.")
             }
         } catch (e) {
-            console.log("네트워크 오류가 발생하였습니다.111")
+            console.log("네트워크 오류가 발생하였습니다.", e)
         }
     }
 
@@ -52,27 +83,39 @@ const ProfileEdit = () => {
         setIsModalOpen(false);
     };
 
-    const isFormValid = phoneNumber.trim() !== ''
-
-    const handleUpdatePhoneNumber = async () => {
-        try {
-            const res = await fetch("http://localhost:4000/api/users", {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ phoneNumber}),
-            });
-
-            if (res.ok) {
-                const result = await res.json();
-                setIsModalOpen(false);
-            } else {
-                console.error("전화번호를 가져오는데 실패하였습니다.")
-            }
-        } catch (e) {
-            console.log("네트워크 오류가 발생하였습니다.")
-        }
+    const handleDeleteModalOpen = () => {
+        setIsDeleteModalOpen(true);
+    }
+    const handleDeleteModalClose = () => {
+        setIsDeleteModalOpen(false);
     }
 
+    const isFormValid = phone.trim() !== ''
+
+
+    const handleUpdatePhoneNumber = async () => {
+        const token = localStorage.getItem('token');
+        try {
+            const res = await fetch("http://localhost:4000/api/profile/phone", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: JSON.stringify({ phone }),
+            });
+
+            if (!res.ok) {
+                throw new Error("전화번호 업데이트 실패");
+            }
+
+            const data = await res.json();  // 응답 데이터를 JSON으로 변환
+            setIsModalOpen(false); // 성공 시 모달을 닫음
+        } catch (error) {
+
+            console.error("Error: ", error);  // 에러 콘솔에 출력
+        }
+    };
     return (
         <div>
             <div className='EditSiteTotal flex flex-col justify-center mt-10'>
@@ -88,28 +131,25 @@ const ProfileEdit = () => {
                         <div className='upSiteNum w-[80%] flex flex-col pt-5'>
                             연락처
                             <input
-                            placeholder='전화번호를 입력해주세요'
+                                placeholder='전화번호를 입력해주세요'
                                 className='mt-3 bg-gray-200 h-10 rounded-lg pl-4'
                                 type='text'
-                                value={phoneNumber}
+                                value={phone}
                                 onChange={(e) => {
                                     const inputValue = e.target.value;
                                     if (/^[0-9+-]*$/.test(inputValue)) {
-                                        setPhoneNumber(inputValue);
+                                        setPhone(inputValue);
                                     }
                                 }}
                             />
                         </div>
-
                         <div className='middleSiteContectNum w-[80%] flex flex-col pt-5'>
                             생년월일
                             <div className='mt-3 bg-gray-100 h-10 rounded-lg pl-4 flex flex-row items-center '>
-                                {birth}
+                                {new Date(birth).toLocaleDateString('en-CA')}
                             </div>
                         </div>
                     </div>
-
-
                     <div className='rightEditSite text-base w-[50%] flex flex-col items-center'>
 
                         <div className='middleSiteBirth w-[80%] flex flex-col '>
@@ -133,6 +173,12 @@ const ProfileEdit = () => {
                         onClick={handleModalOpen}
                         className='flex justify-center items-center align-middle bg-mainColor text-white font-bold text-lg p-4 rounded-lg shadow-lg mt-20'>Update</button>
                 </div>
+                <button
+                    onClick={handleDeleteModalOpen}
+                    className='flex text-gray-400 w-20 h-auto justify-end '>
+                    회원탈퇴
+
+                </button>
             </div>
             <FormModal isOpen={isModalOpen} onClose={handleModalClose}>
                 <div className="text-sm">
@@ -147,10 +193,11 @@ const ProfileEdit = () => {
                     </div>
                 </div>
             </FormModal>
+            <DeleteModal
+                isOpen={isDeleteModalOpen} onClose={handleDeleteModalClose} onDelete={userDelete}/>
         </div>
     )
 }
-
 export default ProfileEdit
 
 
--- src/components/myPage/Schedule.tsx
+++ src/components/myPage/Schedule.tsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PageModal from '../modal/PageModal';
 
+interface Schedule {
+    id: string;
+    title: string;
+    content: string;
+    makePublic: boolean;
+    createAt: string;
+    finishedAt?: string;
+}
+
 const Schedule = () => {
-    const [personerPages, setPersonerPages] = useState("");
-    const [workerPages, setWorkerPages] = useState("");
     const [isModalOpen, setModalOpen] = useState(false);
-    const [savedPersonerTitles, setSavedPersonerTitles] = useState<{title: string; content: string }[]>([]);
-    const [savedWorkTitles, setSavedWorkTitles] = useState<{title: string; content: string }[]>([]);
+    const [savedPersonerTitles, setSavedPersonerTitles] = useState<{ id: string; title: string; content: string }[]>([]);
+    const [savedWorkTitles, setSavedWorkTitles] = useState<{ id: string; title: string; content: string }[]>([]);
     const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
     const [selectedContent, setSelectedContent] = useState<string | null>(null);
     const [modalType, setModalType] = useState<'personal' | 'work' | null>(null);
-
-    const handlethrowData = async()=>{
-
-    }
-
     // 개인 일정과 업무 일정의 페이지네이션 상태 분리
     const [currentPersonalPage, setCurrentPersonalPage] = useState(1);
     const [currentWorkPage, setCurrentWorkPage] = useState(1);
@@ -23,15 +25,113 @@ const Schedule = () => {
     // 체크박스에서 선택된 항목 관리
     const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
-    // 피드백 저장 함수
-    const handleSave = (title: string, content: string) => {
-        const newItem ={title, content};
-        if (modalType === 'personal') {
-            setSavedPersonerTitles((prev) => [...prev, newItem]);
-        } else if (modalType === 'work') {
-            setSavedWorkTitles((prev) => [...prev,newItem]);
+    useEffect(() => {
+        MySchedule();
+    }, []);
+
+    const MySchedule = async () => {
+        const token = localStorage.getItem('token');
+        try {
+            const res = await fetch("http://localhost:4000/api/schedule/user", {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+            if (!res.ok) {
+                throw new Error("일정 조회에 실패하였습니다.");
+            }
+
+            const data: Schedule[] = await res.json();
+            const personalSchedule = data
+                .filter((item: Schedule) => !item.makePublic)
+                .map(({ id, title, content }) => ({ id, title, content }));
+            const workSchedule = data
+                .filter((item: Schedule) => item.makePublic)
+                .map(({ id, title, content }) => ({ id, title, content }));
+
+            setSavedPersonerTitles(personalSchedule);
+            setSavedWorkTitles(workSchedule);
+        } catch (e) {
+            console.error('에러 발생:', e);
         }
-        setModalOpen(false);
+    };
+
+    // 피드백 저장 함수
+    const handleSave = async (title: string, content: string) => {
+        const makePublic = modalType === 'work';
+        const token = localStorage.getItem('token');
+
+        try {
+            const res = await fetch("http://localhost:4000/api/schedule", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    title,
+                    content,
+                    makePublic,
+                    createdAt: new Date().toISOString(),
+                    finishedAt: "",
+                }),
+            });
+
+            if (!res.ok) {
+                throw new Error("보내기가 실패하였습니다.");
+            }
+
+            const data = await res.json();
+            console.log("서버 응답:", data);
+
+            // 서버 응답이 성공적일 때 상태 업데이트 및 모달 닫기
+            if (makePublic) {
+                setSavedWorkTitles((prev) => [...prev, { id: data.id, title, content }]);
+            } else {
+                setSavedPersonerTitles((prev) => [...prev, { id: data.id, title, content }]);
+            }
+
+            setModalOpen(false);
+        } catch (e) {
+            console.error("에러 발생:", e);
+        }
+    };
+
+    // 일정 삭제 함수
+    const handleDeletePersoner = async (scheduleId: string, index: number, type: 'personal' | 'work') => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                console.error("토큰이 없습니다.");
+                return;
+            }
+
+            const res = await fetch('http://localhost:4000/api/schedule/user/:scheduleId', {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+
+            if (!res.ok) {
+                console.error("삭제 실패하였습니다.", res.status);
+                return;
+            }
+
+            console.log("삭제 성공");
+
+            // 로컬 상태 업데이트
+            if (type === 'personal') {
+                setSavedPersonerTitles((prev) => prev.filter((_, i) => i !== index));
+            } else if (type === 'work') {
+                setSavedWorkTitles((prev) => prev.filter((_, i) => i !== index));
+            }
+        } catch (e) {
+            console.error('오류가 발생하였습니다.', e);
+        }
     };
 
     // 공지사항 항목 클릭 시 내용 보기
@@ -57,13 +157,6 @@ const Schedule = () => {
         setModalOpen(true);
     };
 
-    const handleDeleteClickPersoner = (index: number) => {
-        setSavedPersonerTitles((prev) => prev.filter((_, i) => i !== index));
-    };
-    const handleDeleteClickWork = (index: number) => {
-        setSavedWorkTitles((prev) => prev.filter((_, i) => i !== index));
-    };
-
     // 체크박스 변경 함수
     const handleCheckboxChange = (index: number) => {
         setSelectedIndex(index === selectedIndex ? null : index);
@@ -80,7 +173,6 @@ const Schedule = () => {
     const indexOfFirstWorkItem = indexOfLastWorkItem - itemsPerPage;
     const currentWorkItems = savedWorkTitles.slice(indexOfFirstWorkItem, indexOfLastWorkItem);
     const totalWorkPages = Math.ceil(savedWorkTitles.length / itemsPerPage);
-
 
     return (
         <div className="sectionDevide h-[30rem] flex flex-row items-center justify-center">
@@ -102,9 +194,9 @@ const Schedule = () => {
                     </div>
                     <ul>
                         {savedPersonerTitles.length > 0 ? (
-                            currentPersonalItems.map(({ title, content }, index) => (
+                            currentPersonalItems.map(({ id, title, content }, index) => (
                                 <li
-                                    key={index}
+                                    key={id || index}
                                     className="cursor-default text-lg w-[90%] m-5 border-b flex items-center group"
                                 >
                                     <input
@@ -120,7 +212,7 @@ const Schedule = () => {
                                         {title}
                                     </div>
                                     <button
-                                        onClick={() => handleDeleteClickPersoner(indexOfFirstPersonalItem + index)}
+                                        onClick={() => handleDeletePersoner(id, indexOfFirstPersonalItem + index, 'personal')}
                                         className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 mr-4"
                                     >
                                         ❌
@@ -138,9 +230,8 @@ const Schedule = () => {
                             {Array.from({ length: totalPersonalPages }, (_, i) => (
                                 <button
                                     key={i}
-                                    className={`mx-1 px-4 py-2 rounded ${
-                                        currentPersonalPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300'
-                                    }`}
+                                    className={`mx-1 px-4 py-2 rounded ${currentPersonalPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300'
+                                        }`}
                                     onClick={() => setCurrentPersonalPage(i + 1)}
                                 >
                                     {i + 1}
@@ -148,7 +239,6 @@ const Schedule = () => {
                             ))}
                         </div>
                     )}
-           
                 </div>
             </div>
 
@@ -167,12 +257,11 @@ const Schedule = () => {
                             }}
                         />
                     </div>
-
                     <ul>
                         {savedWorkTitles.length > 0 ? (
-                            currentWorkItems.map(({title, content }, index) => (
+                            currentWorkItems.map(({ id, title, content }, index) => (
                                 <li
-                                    key={index}
+                                    key={id || index}
                                     className="cursor-default text-lg w-[90%] m-5 border-b flex items-center group"
                                 >
                                     <div
@@ -182,7 +271,7 @@ const Schedule = () => {
                                         {title}
                                     </div>
                                     <button
-                                        onClick={() => handleDeleteClickWork(indexOfFirstWorkItem + index)}
+                                        onClick={() => handleDeletePersoner(id, indexOfFirstWorkItem + index, 'work')}
                                         className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300 mr-4"
                                     >
                                         ❌
@@ -200,9 +289,8 @@ const Schedule = () => {
                             {Array.from({ length: totalWorkPages }, (_, i) => (
                                 <button
                                     key={i}
-                                    className={`mx-1 px-4 py-2 rounded ${
-                                        currentWorkPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300'
-                                    }`}
+                                    className={`mx-1 px-4 py-2 rounded ${currentWorkPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300'
+                                        }`}
                                     onClick={() => setCurrentWorkPage(i + 1)}
                                 >
                                     {i + 1}
@@ -214,13 +302,11 @@ const Schedule = () => {
             </div>
 
             <PageModal
-            
                 isOpen={isModalOpen}
                 onClose={() => setModalOpen(false)}
                 onSave={handleSave}
                 title={selectedTitle ?? ''}
                 content={selectedContent ?? ''}
-                
             />
         </div>
     );
